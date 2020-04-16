7ac3f529d08d6bf3a5a59039e5c18cf762112ebc
KinG <460715104@qq.com>
2020-04-16 15:37:10 +0800

    练习Python

---
+++ First-2.py
@@ -0,0 +1,23 @@
+famous_person="albert einstein"
+b=" once said,"
+a='"A oerson who never made a mistake never tried anything new."'
+print(famous_person.title()+b+a)
+
+famous_person=" albert "
+a="einstein"
+print(famous_person.strip()+a)
+
+famous_person="albert "
+a="einstein"
+print(famous_person.rstrip()+a)
+
+famous_person=" albert einstein"
+print(famous_person.lstrip())
+
+famous_person=" albert "
+a="\neinstein"
+b="\tyes"
+print(famous_person.strip()+a+b)
+
+a=2+3
+print(a)
